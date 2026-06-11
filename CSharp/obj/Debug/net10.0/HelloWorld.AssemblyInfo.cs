88bcd00e7a653389cdd723c8329223a53124efaf
Zinnn <zinnnwastaken@proton.me>
2026-06-11 15:44:18 +0700

    Added ASSembly

--- CSharp/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ CSharp/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e25fe700bce593fbc676797539a7fdb29f8865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682daf3af59bd4170c78b5bedb29f393f6e153bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
